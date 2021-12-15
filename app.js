03af5c25a401e920a817ae1cdd4bb151863b2f04
Matthieu Dubo <matthieu.dubo40@gmail.com>
2021-12-15 15:38:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+const cors = require("cors");
+require('dotenv').config();
+
+const port = process.env.PORT || 8000;
+app.use(express.json());
+
+const corsOptions = {
+  origin: true,
+  credentials: true, // access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.post('/send', (req, res) => {
+  const { name, subject, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+  
+  const mailOptions = {
+    from: 'youremail@gmail.com',
+    to: process.env.EMAIL,
+    subject: subject,
+    text: `Name: ${name}, email: ${email}
+    Message: ${message}`
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
